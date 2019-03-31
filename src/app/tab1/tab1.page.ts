ee1f0ac963c3656c25e7e005d78e91c615b19297
Eddy Jo <edwardjo811@gmail.com>
2019-03-31 19:35:53 -0400

    added slide and segment

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  @ViewChild('slider') slider: IonSlides;
+  page = "0";
+  
+  selectedTab(index) {
+      this.slider.slideTo(index);
+  }
+  
+  async moveButton() {
+      let index = await this.slider.getActiveIndex();
+      this.page = index.toString();
+  } 
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
+}
